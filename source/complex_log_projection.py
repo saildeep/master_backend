2c19cd6ce427c6938172903fefc65c9bc759f59f
Jonas Körner <jkoerner@odd-it.com>
2020-05-08 13:39:23 +0200

    Added preprojection to params

--- source/complex_log_projection.py
+++ source/complex_log_projection.py
@@ -1,8 +1,14 @@
 from source.lat_lng import LatLng
+from source.preprojections import LambertAzimuthalEqualArea, AbstractPreprojection
 
 
 class ComplexLogProject():
-    def __init__(self, center1: LatLng, center2: LatLng, smoothing_angle_radians: float):
+    def __init__(self,
+                 center1: LatLng,
+                 center2: LatLng,
+                 smoothing_angle_radians: float,
+                 preprojection: AbstractPreprojection = LambertAzimuthalEqualArea):
         self.center1 = center1
         self.center2 = center2
         self.smoothing_angle = smoothing_angle_radians
+        self.preprojection = preprojection
