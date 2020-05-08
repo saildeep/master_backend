27f0bd5ec9ff34b22d49dd1357600bf0eaf0d8f8
Jonas Körner <jkoerner@odd-it.com>
2020-05-08 12:52:37 +0200

    Added tests from d3 data

--- test/test_preprojections.py
+++ test/test_preprojections.py
@@ -2,27 +2,39 @@ import unittest
 import numpy as np
 from source import preprojections
 
+input_data = np.transpose([
+    [0, 0],
+    [45, 0],
+    [0, 45],
+    [-45, 45]
+])
+
 
 class TestPreprojections(unittest.TestCase):
 
     def test_lambertEqualArea(self):
-        input_data = np.array([
-            [0, 45], [0, 0]
-        ])
-
         projection = preprojections.LambertAzimuthalEqualArea()
         output_data = projection(input_data)
         np.testing.assert_almost_equal(output_data[:, 0], np.array([0, 0]))  # assert center has not moved
         offcenter_point = output_data[:, 1]
         self.assertTrue(offcenter_point[0] != 0 or offcenter_point[1] != 0)  # assert other point is of center
 
-    def test_lambertEqualDistance(self):
-        input_data = np.array([
-            [0, 45], [0, 0]
-        ])
+        desired_output = np.transpose([[0, 0],
+                                       [0.7653668647301796, 0],
+                                       [0, 0.7653668647301796],
+                                       [-0.5773502691896257, 0.8164965809277259]])
 
+        np.testing.assert_almost_equal(output_data, desired_output)
+
+    def test_lambertEqualDistance(self):
         projection = preprojections.LambertAzimuthalEqualDistance()
         output_data = projection(input_data)
         np.testing.assert_almost_equal(output_data[:, 0], np.array([0, 0]))  # assert center has not moved
         offcenter_point = output_data[:, 1]
         self.assertTrue(offcenter_point[0] != 0 or offcenter_point[1] != 0)  # assert other point is of center
+
+        desired_output = np.transpose([[0, 0],
+                                       [0.7853981633974482, 0],
+                                       [0, 0.7853981633974482],
+                                       [-0.6045997880780726, 0.8550332201079093]])
+        np.testing.assert_almost_equal(output_data, desired_output)
