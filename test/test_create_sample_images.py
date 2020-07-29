e58445e88d83467e83c9d60d3abc3b8123a94532
Jonas Körner <jkoerner@odd-it.com>
2020-07-29 12:00:04 +0200

    Settings for sample creation

--- test/test_create_sample_images.py
+++ test/test_create_sample_images.py
@@ -38,12 +38,12 @@ class CreateSampleImages(TestCase):
             #frankfurt_a_m = LatLng(50.115822, 8.702537)
             angle = 30
             hamburg = LatLng(53.559988,9.982358)
-            hamburg_elbstrand = LatLng(53.544545,9.895165)
+            hamburg_elbbruecken = LatLng(53.535251,10.020135)
             lueneburg = LatLng(53.245280,10.408478)
             hannover = LatLng(52.370487,9.724743)
             fulda = LatLng(50.527068,9.684608)
             stockach = LatLng(47.847596,9.007671)
-            for i,to in enumerate([ hamburg_elbstrand,lueneburg,hannover,fulda,stockach]):
+            for i,to in enumerate([ hamburg_elbbruecken,lueneburg,hannover,fulda,stockach]):
 
                 projection = ComplexLogProjection(hamburg, to, math.radians(angle),
                                                   smoothing_function_type=DualCosSmoothingFunction)
@@ -55,7 +55,7 @@ class CreateSampleImages(TestCase):
 
                 im = Image.alpha_composite(d_mapbox,d_trans)
                 dist = int(hamburg.distanceTo(to))
-                filename = "sample-distance-" + dist+".png"
+                filename = "sample-distance-" + str(dist)+".png"
                 im.save(filename)
 
                 print("Finished " + filename + " with distance " + str(dist))
