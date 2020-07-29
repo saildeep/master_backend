26905c64cb1bbf1d512975d96448312eed639c7d
Jonas Körner <jkoerner@odd-it.com>
2020-07-29 10:23:23 +0200

    Added test for generating distance examples

--- source/lat_lng.py
+++ source/lat_lng.py
@@ -1,3 +1,5 @@
+import math
+
 class LatLng():
     def __init__(self, lat: float, lng: float):
         self.lat = lat
@@ -10,3 +12,20 @@ class LatLng():
 
     def __str__(self):
         return "LatLng:(" + str(self.lat) + "," + str(self.lng) + ")"
+
+    def distanceTo(self,other):
+        lat1 = self.lat
+        lon1 = self.lng
+        lat2 = other.lat
+        lon2 = other.lng
+        radius = 6371  # km
+
+        dlat = math.radians(lat2 - lat1)
+        dlon = math.radians(lon2 - lon1)
+        a = math.sin(dlat / 2) * math.sin(dlat / 2) + math.cos(math.radians(lat1)) \
+            * math.cos(math.radians(lat2)) * math.sin(dlon / 2) * math.sin(dlon / 2)
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+        d = radius * c
+
+        return d
+
--- test/test_create_sample_images.py
+++ test/test_create_sample_images.py
@@ -10,19 +10,54 @@ from source.smoothing_functions import DualCosSmoothingFunction
 from PIL import Image
 
 class CreateSampleImages(TestCase):
-    def test_project_image_osm_wide(self):
+    def test_project_image_angles(self):
         prov = get_providers()
-        for angle in [0,10,20,30,40]:
-            frankfurt_a_m = LatLng(50.115822, 8.702537)
-            leipzig = LatLng(51.348419, 12.370946)  #
-            projection = ComplexLogProjection(frankfurt_a_m, leipzig, math.radians(angle),
+        trange = TargetSectionDescription(-math.pi * 2, math.pi * 2, 4000, -math.pi/2, math.pi/2, 1000)
+        #frankfurt_a_m = LatLng(50.115822, 8.702537)
+        halle = LatLng(51.506136,11.964422)
+
+        leipzig = LatLng(51.348419, 12.370946)  #
+        for angle in [0,15,30,45]:
+
+            projection = ComplexLogProjection(halle, leipzig, math.radians(angle),
                                               smoothing_function_type=DualCosSmoothingFunction)
-            projector = RasterProjector(projection, prov['transparent'])
-            trange = TargetSectionDescription(-math.pi * 3, math.pi * 3, 2000, -math.pi, math.pi, 500)
-            d = projector.project(trange)
-            im = Image.fromarray(d)
+            projector_transparent = RasterProjector(projection, prov['transparent'])
+            projector_mapbox = RasterProjector(projection, prov['mapbox'])
+
+            d_trans =  Image.fromarray(projector_transparent.project(trange))
+            d_mapbox = Image.fromarray(projector_mapbox.project(trange))
+
+            im = Image.alpha_composite(d_mapbox,d_trans)
             filename = "sample-angle-" + str(angle)+".png"
             im.save(filename)
-            print("Finished " + filename)
+            print(filename)
+
+    def test_project_image_distances(self):
+            prov = get_providers()
+            trange = TargetSectionDescription(-math.pi * 2, math.pi * 2, 4000, -math.pi, math.pi, 2000)
+            #frankfurt_a_m = LatLng(50.115822, 8.702537)
+            angle = 30
+            hamburg = LatLng(53.559988,9.982358)
+            hamburg_elbstrand = LatLng(53.544545,9.895165)
+            lueneburg = LatLng(53.245280,10.408478)
+            hannover = LatLng(52.370487,9.724743)
+            fulda = LatLng(50.527068,9.684608)
+            stockach = LatLng(47.847596,9.007671)
+            for i,to in enumerate([ hamburg_elbstrand,lueneburg,hannover,fulda,stockach]):
+
+                projection = ComplexLogProjection(hamburg, to, math.radians(angle),
+                                                  smoothing_function_type=DualCosSmoothingFunction)
+                projector_transparent = RasterProjector(projection, prov['transparent'])
+                projector_mapbox = RasterProjector(projection, prov['mapbox'])
+
+                d_trans =  Image.fromarray(projector_transparent.project(trange))
+                d_mapbox = Image.fromarray(projector_mapbox.project(trange))
+
+                im = Image.alpha_composite(d_mapbox,d_trans)
+                dist = int(hamburg.distanceTo(to))
+                filename = "sample-distance-" + dist+".png"
+                im.save(filename)
+
+                print("Finished " + filename + " with distance " + str(dist))
 
 
