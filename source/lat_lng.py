f09d2efad1585961b1e00e07150026f78234628d
Jonas Körner <jkoerner@odd-it.com>
2020-09-10 10:33:59 +0200

    Using more accurate midpoint

--- source/lat_lng.py
+++ source/lat_lng.py
@@ -1,4 +1,8 @@
+from typing import List
+from warnings import warn
+
 import math
+
 
 class LatLng():
     def __init__(self, lat: float, lng: float):
@@ -13,7 +17,7 @@ class LatLng():
     def __str__(self):
         return "LatLng:(" + str(self.lat) + "," + str(self.lng) + ")"
 
-    def distanceTo(self,other):
+    def distanceTo(self, other):
         lat1 = self.lat
         lon1 = self.lng
         lat2 = other.lat
@@ -29,8 +33,43 @@ class LatLng():
 
         return d
 
+    @staticmethod
+    def midpoint(positions:List['LatLng'])->'LatLng':
+        if len(positions) < 1:
+            raise Exception("Could not calculate midpoint of single coordinate")
+        if len(positions) ==1:
+            warn("Calculating midpoint of 1 element")
+            return positions[0]
+
+        x = 0
+        y = 0
+        z = 0
+        w = 1.0 / float(len(positions))
+        for platlng in positions:
+            p = platlng.spherical_coordinate
+            x += p[0] * w
+            y += p[1] * w
+            z += p[2] * w
+
+        return LatLng.from_3d(x,y,z)
 
 
-    def approxMidpoint(self,other):
-        return LatLng((self.lat + other.lat)*.5,(self.lng + other.lng) * .5 )
 
+    @staticmethod
+    def from_3d(x,y,z)->'LatLng':
+        lng = math.atan2(y,x)
+        hyp = math.sqrt(x*x+y*y)
+        lat = math.atan2(z,hyp)
+        return LatLng(math.degrees(lat),math.degrees(lng))
+
+    @property
+    def spherical_coordinate(self):
+        lat = math.radians(self.lat)
+        lng = math.radians(self.lng)
+        x = math.cos(lat) * math.cos(lng)
+        y = math.cos(lat) * math.sin(lng)
+        z = math.sin(lat)
+        return (x, y, z)
+
+    def approxMidpoint(self, other):
+        return LatLng((self.lat + other.lat) * .5, (self.lng + other.lng) * .5)
