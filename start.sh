76794a22b94c14b05c635973a373a991af6022cd
Jonas Körner <jkoerner@odd-it.com>
2020-09-16 09:56:11 +0200

    Allowed caching of projected cities in memcache

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-memcached -s /tmp/memcached.sock -a 775 -v --memory-limit=8192 --threads=16 > /tmp/memcached.log &
+memcached -s /tmp/memcached.sock -a 775 -v --memory-limit=8192 --threads=16 -I 16m > /tmp/memcached.log &
 uwsgi --ini /app/uwsgi.ini
