dff46347beb07a7e244d98aee5e1c1b750f808da
Jonas Körner <jkoerner@odd-it.com>
2020-11-04 15:57:17 +0100

    Styling

--- test/test_create_presentation_images.py
+++ test/test_create_presentation_images.py
@@ -18,7 +18,7 @@ def save_plot(fig,name):
     fig.savefig(os.path.join(folder,name+'.png'),dpi=500)
 
 
-fig_w = 5
+fig_h = 5
 center_positions= [(2,1.5),(4,3)]
 route = [center_positions[0],(2.2,2.5),(3.5,2.4),center_positions[1]]
 
@@ -26,29 +26,47 @@ midpoint = ((center_positions[0][0] + center_positions[1][0])*.5,(center_positio
 
 center_distance = math.sqrt( (center_positions[0][0]- center_positions[1][0])**2 + (center_positions[0][1]- center_positions[1][1])**2 )
 
+
+
+displayable_area_style={
+    "color":'blue',
+    "alpha":0.3,
+    "zorder":-1,
+}
+
+center_point_color = "orange"
+midpoint_color = "green"
+midline_style = {"color":"cyan","linestyle":"dashed"}
+route_color="red"
+
+
+
+
+
+
 class TestCreatePresentationImages(TestCase):
 
 
 
 
     def add_centers(self,ax):
-        ax.scatter(list(map(lambda x: x[0], center_positions)), list(map(lambda x: x[1], center_positions)))
+        ax.scatter(list(map(lambda x: x[0], center_positions)), list(map(lambda x: x[1], center_positions)),color=center_point_color)
 
         for i, cp in enumerate(center_positions):
             x = cp[0]
             y = cp[1]
-            ax.annotate("$~G^{}$".format(i + 1), (x, y), va="top", ha='left')
+            ax.annotate("$~G^{}$".format(i + 1), (x, y), va="top", ha='left',color=center_point_color)
 
 
     def add_midline(self,ax):
         ax.plot([center_positions[0][0], center_positions[1][0]], [center_positions[0][1], center_positions[1][1]],
-                linestyle='dashed')
-        ax.scatter([midpoint[0]], [midpoint[1]], color='black')
-        ax.annotate("$~M$", midpoint, va="top", ha='left')
+                **midline_style)
+        ax.scatter([midpoint[0]], [midpoint[1]], color=midpoint_color)
+        ax.annotate("$~M$", midpoint,color=midpoint_color, va="top", ha='left')
 
     def add_range_circles(self,ax):
-        ax.add_patch(plt.Circle(center_positions[0], center_distance / 2, color='orange', alpha=0.5, zorder=-1))
-        ax.add_patch(plt.Circle(center_positions[1], center_distance / 2, color='orange', alpha=0.5, zorder=-1))
+        ax.add_patch(plt.Circle(center_positions[0], center_distance / 2, **displayable_area_style))
+        ax.add_patch(plt.Circle(center_positions[1], center_distance / 2, **displayable_area_style))
 
     def add_modified_range_circles(self,ax,angle=30):
         smoothing = DualCosSmoothingFunction(math.radians(angle))
@@ -66,7 +84,7 @@ class TestCreatePresentationImages(TestCase):
 
             polygon = np.array(polygon)
 
-            ax.add_patch(plt.Polygon(np.array(polygon),color='blue',alpha=.5))
+            ax.add_patch(plt.Polygon(np.array(polygon),**displayable_area_style))
 
 
 
@@ -82,7 +100,7 @@ class TestCreatePresentationImages(TestCase):
         ax.plot(x,y,color='red')
 
     def get_new_fig(self):
-        fig = plt.figure()
+        fig = plt.figure(figsize=(fig_h,fig_h))
 
         ax = plt.gca()
         ax.set_aspect('equal')
@@ -109,7 +127,7 @@ class TestCreatePresentationImages(TestCase):
 
         fig, ax = self.get_new_fig()
         self.add_centers(ax)
-        self.add_route(ax)
+        #self.add_route(ax)
         self.add_midline(ax)
         save_plot(fig, 'midpoint')
 
@@ -117,16 +135,37 @@ class TestCreatePresentationImages(TestCase):
 
         fig, ax = self.get_new_fig()
         self.add_centers(ax)
-        self.add_route(ax)
+        #self.add_route(ax)
         self.add_midline(ax)
         self.add_modified_range_circles(ax, angle=0)
         save_plot(fig, 'circles')
+
+    def test_create_circles_affine(self):
+        fig,axs = plt.subplots(1,3,figsize=(fig_h*1.8,fig_h))
+        ax = axs[0]
+        ax.set_aspect('equal')
+        self.add_centers(ax)
+        # self.add_route(ax)
+        self.add_midline(ax)
+        self.add_modified_range_circles(ax, angle=0)
+
+        for i,ax in zip([0,1],[axs[1],axs[2]]):
+            ax.set_aspect('equal')
+            ax.scatter(0,0,color=center_point_color)
+            ax.annotate("$~G^{}$".format(i + 1), (0, 0), va="bottom", ha='right',color=center_point_color)
+            ax.scatter(1,0,color=midpoint_color)
+            ax.annotate("$M$",(1,0), va="bottom", ha='right',color=midpoint_color)
+            ax.add_patch(plt.Circle((0,0), 1, **displayable_area_style))
+            ax.plot([0,1],[0,0],**midline_style)
+
+
+        save_plot(fig,'circles_affine')
 
     def test_create_smoothed_circles(self):
 
         fig, ax = self.get_new_fig()
         self.add_centers(ax)
-        self.add_route(ax)
+        #self.add_route(ax)
         self.add_midline(ax)
         self.add_modified_range_circles(ax, angle=30)
         save_plot(fig, 'circles_smoothed')
